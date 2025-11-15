fb0aae1079cf5c115ed67af316f5038ea441bb0a
abrioness <87965221+abrioness@users.noreply.github.com>
2025-11-15 11:45:48 -0600

    Add empty line for code readability

--- Program.cs
+++ Program.cs
@@ -10,11 +10,12 @@ namespace WebColegio
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            
             // Add services to the container.
             //builder.Services.AddRazorPages();
             builder.Services.AddScoped<IServicesApi, ServicesApi>();
             builder.Services.AddControllersWithViews();
+            builder.Configuration["ApiUrl"] = Environment.GetEnvironmentVariable("API_URL");
             //builder.Services.AddCors(options =>
             //{
             //    options.AddDefaultPolicy(policy =>
